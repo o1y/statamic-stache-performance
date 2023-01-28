d31b523a497e41f945b2a34c722398d1c61f2376
Oliver Mesieh <olivermesieh@gmail.com>
2023-01-28 23:45:56 +0100

    Add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+use Statamic\Facades\Entry;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Collection::times(5000, function () {
+            return Entry::make()
+                ->collection('pages')
+                ->data(['title' => 'Lorem Ipsum', 'content' => 'Lorem Ipsum'])
+                ->published(true)
+                ->save();
+        });
     }
 }
